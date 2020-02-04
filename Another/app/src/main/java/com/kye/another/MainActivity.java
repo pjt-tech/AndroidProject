9b670f932482c4091d051ec2e73d275a39b9f02c
unknown <dktkd7044@gmail.com>
2020-02-04 14:09:20 +0900

    aa

--- Another/app/src/main/java/com/kye/another/MainActivity.java
+++ Another/app/src/main/java/com/kye/another/MainActivity.java
@@ -2,13 +2,20 @@ package com.kye.another;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        intent = getIntent(); //get을 통해 받음
+        String name = intent.getStringExtra("name"); //get을 통해 받음
+        Toast.makeText(getApplicationContext(),"보낸 Activity : " + name,Toast.LENGTH_LONG).show();
     }
 }
