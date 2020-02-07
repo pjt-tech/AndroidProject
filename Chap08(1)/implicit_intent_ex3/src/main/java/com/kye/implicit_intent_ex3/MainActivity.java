3797457bc2b4af8fd020f898e43dbe67fb164dca
unknown <dktkd7044@gmail.com>
2020-02-07 11:52:37 +0900

    aa

---
+++ Chap08(1)/implicit_intent_ex3/src/main/java/com/kye/implicit_intent_ex3/MainActivity.java
@@ -0,0 +1,22 @@
+package com.kye.implicit_intent_ex3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView)findViewById(R.id.web);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_assets/sample.html");
+    }
+}
