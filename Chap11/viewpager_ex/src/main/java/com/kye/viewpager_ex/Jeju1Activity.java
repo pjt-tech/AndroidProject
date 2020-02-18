0e616ca78b66af34311a2764cb661baa4cc65dac
unknown <dktkd7044@gmail.com>
2020-02-18 11:51:34 +0900

    aaa

---
+++ Chap11/viewpager_ex/src/main/java/com/kye/viewpager_ex/Jeju1Activity.java
@@ -0,0 +1,14 @@
+package com.kye.viewpager_ex;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Jeju1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jeju1);
+    }
+}
--- Chap11/viewpager_ex/src/main/java/com/kye/viewpager_ex/Person.java
+++ Chap11/viewpager_ex/src/main/java/com/kye/viewpager_ex/Person.java
@@ -2,7 +2,10 @@ package com.kye.viewpager_ex;
 
 
         import android.content.Context;
+        import android.content.Intent;
+        import android.net.Uri;
         import android.view.LayoutInflater;
+        import android.view.View;
         import android.widget.Button;
         import android.widget.ImageView;
         import android.widget.LinearLayout;
@@ -23,11 +26,33 @@ public class Person extends LinearLayout {
     }
 
     public void init(){
-        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.activity_pager,this,true);
         textView = findViewById(R.id.textView);
         button = findViewById(R.id.button);
         imageView = findViewById(R.id.imageView);
+
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String call_Num = (String)button.getTag();
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel : " +call_Num));
+                context.startActivity(intent);
+            }
+        });
+
+        imageView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (imageId){
+                    case R.drawable.jeju1 :
+                        Intent intent = new Intent(context.getApplicationContext(),Jeju1Activity.class);
+                        context.startActivity(intent);
+                        break;
+                }
+            }
+        });
+
     }
 
     public void setName(String name) {
