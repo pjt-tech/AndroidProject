9a63a0721356bf4c714d8d70f4477cd96c57f00e
unknown <dktkd7044@gmail.com>
2020-02-19 11:23:56 +0900

    music

--- Chap11/service_ex/src/main/java/com/kye/service_ex/MainActivity.java
+++ Chap11/service_ex/src/main/java/com/kye/service_ex/MainActivity.java
@@ -2,14 +2,17 @@ package com.kye.service_ex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText editText;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +21,35 @@ public class MainActivity extends AppCompatActivity {
 
         editText = findViewById(R.id.editText);
         Button button = findViewById(R.id.button);
+        Button button2 = findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String name = editText.getText().toString();
+                Intent intent = new Intent(getApplicationContext(),MyService.class);
+                intent.putExtra("name",name);
+                startService(intent);
 
             }
         });
 
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(getApplicationContext(),MyService.class);
+               stopService(intent);
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if(intent!=null) {
+            String name = intent.getStringExtra("name");
+            Toast.makeText(getApplicationContext(), name, Toast.LENGTH_LONG).show();
+        }
     }
 }
--- Chap11/service_ex/src/main/java/com/kye/service_ex/MyService.java
+++ Chap11/service_ex/src/main/java/com/kye/service_ex/MyService.java
@@ -3,6 +3,7 @@ package com.kye.service_ex;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class MyService extends Service {
     public MyService() {
@@ -12,5 +13,30 @@ public class MyService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("MyService","onCreate 호출");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("MyService","onStartCommand 호출");
+        String name = intent.getStringExtra("name");
+        Log.d("MyService","name : "+name );
+
+        intent= new Intent(getApplicationContext(),MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("name",name+" from service");
+        startActivity(intent);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d("MyService","onDestroy 호출");
+        super.onDestroy();
     }
 }
