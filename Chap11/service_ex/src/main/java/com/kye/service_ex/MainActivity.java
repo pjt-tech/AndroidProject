3ee7f1974d002b4aea826351ec07ec7897b5391b
unknown <dktkd7044@gmail.com>
2020-02-19 10:37:03 +0900

    aa

---
+++ Chap11/service_ex/src/main/java/com/kye/service_ex/MainActivity.java
@@ -0,0 +1,30 @@
+package com.kye.service_ex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+    }
+}
