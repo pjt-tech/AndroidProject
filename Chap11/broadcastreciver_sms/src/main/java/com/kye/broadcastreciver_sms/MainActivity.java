7d83616f5334c9311a28ce627a6564ec96f5912f
unknown <dktkd7044@gmail.com>
2020-02-24 09:58:09 +0900

    a

--- Chap11/broadcastreciver_sms/src/main/java/com/kye/broadcastreciver_sms/MainActivity.java
+++ Chap11/broadcastreciver_sms/src/main/java/com/kye/broadcastreciver_sms/MainActivity.java
@@ -1,14 +1,32 @@
 package com.kye.broadcastreciver_sms;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.pedro.library.AutoPermissionsListener;
+
+public class MainActivity extends AppCompatActivity implements AutoPermissionsListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onDenied(int i, String[] strings) {
+
+    }
+
+    @Override
+    public void onGranted(int i, String[] strings) {
+
     }
 }
--- Chap11/broadcastreciver_sms/src/main/java/com/kye/broadcastreciver_sms/SmsReceiver.java
+++ Chap11/broadcastreciver_sms/src/main/java/com/kye/broadcastreciver_sms/SmsReceiver.java
@@ -3,13 +3,48 @@ package com.kye.broadcastreciver_sms;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+import java.sql.Date;
 
 public class SmsReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        Log.d("SMS Reciver","onReceive() 호출됨");
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] messages = parseSmsMessage(bundle);
+        if(messages!=null && messages.length>0){
+            String sender = messages[0].getOriginatingAddress(); //발신번호
+            Log.d("SMS Reciver","SMS Sender"+ sender);
+
+            String contents = messages[0].getDisplayMessageBody(); //발신메시지
+            Log.d("SMS Reciver","SMS contents"+ contents);
+
+            Date recevedDate = new Date(messages[0].getTimestampMillis()); //수신시간
+            Log.d("SMS Reciver","SMS recevedDate"+ recevedDate);
+        }
+    }
+
+    private SmsMessage[] parseSmsMessage(Bundle bundle){
+        Object[] object = (Object[])bundle.get("pdus");
+        //sms 데이터를 처리하는 SMTP가 있는데 그 안에 pdus라는 이름으로 sms 정보들이 들어가 있음
+        //실제 메시지는 object[]에 pdus 형식으로 저장되어 있음
+        SmsMessage[] messages = new SmsMessage[object.length];
+        //sms를 받아올 SmsMessage 배열을 만듬
+        for(int i= 0; i<object.length; i++){
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                String format = bundle.getString("format");
+                messages[i] = SmsMessage.createFromPdu((byte[]) object[i],format);
+                //각 메시지를 추출
+            }else {
+                messages[i] = SmsMessage.createFromPdu((byte[])object[i]);
+            }
+        }
+        return  messages;
     }
 }
