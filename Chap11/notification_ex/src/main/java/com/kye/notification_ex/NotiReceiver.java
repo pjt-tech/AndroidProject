29c3282031e5ce0396b798953458cf380ceef221
unknown <dktkd7044@gmail.com>
2020-02-27 11:47:39 +0900

    a

---
+++ Chap11/notification_ex/src/main/java/com/kye/notification_ex/NotiReceiver.java
@@ -0,0 +1,17 @@
+package com.kye.notification_ex;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class NotiReceiver extends BroadcastReceiver {
+
+    @Override
+
+    public void onReceive(Context context, Intent intent) {
+        Toast toast=Toast.makeText(context, "I am NotiReceiver", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
