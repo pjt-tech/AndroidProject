2b6ca79e5ab2c49946e4e034ae998b4a3d13aecd
unknown <dktkd7044@gmail.com>
2020-04-20 14:43:24 +0900

    a

--- Chap_Database/app/src/androidTest/java/com/kye/chap_database/ExampleInstrumentedTest.java
+++ Chap_Database/app/src/androidTest/java/com/kye/chap_database/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.kye.chap_database;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
--- Chap_Database/app/src/main/java/com/kye/chap_database/MainActivity.java
+++ Chap_Database/app/src/main/java/com/kye/chap_database/MainActivity.java
@@ -1,13 +1,69 @@
 package com.kye.chap_database;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import android.database.sqlite.SQLiteDatabase;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText db_Name,tb_Name;
+    TextView status;
+
+    String database_name,table_name;
+    Boolean database_create = false;
+    Boolean table_create = false;
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db_Name = findViewById(R.id.edtDbName);
+        tb_Name = findViewById(R.id.edtTbName);
+        status = findViewById(R.id.status);
+
+        Button database_btn = findViewById(R.id.createDb);
+        Button table_btn = findViewById(R.id.createTb);
+
+        database_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                database_name = db_Name.getText().toString();
+                createDatabase(database_name);
+            }
+        });
+
+        table_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    public void insert_Click(View view) {
+    }
+
+    public void update_Click(View view) {
+    }
+
+    public void delete_Click(View view) {
+    }
+    public void createDatabase(String name) {
+        if (database_create == false) {
+            db = openOrCreateDatabase(name, MODE_PRIVATE, null);
+            database_create = true;
+            println("[" + name + "] 데이터베이스 생성이 완료되었습니다.");
+        }
+    }
+    public void println(String msg){
+        status.append("\n" + msg);
     }
 }
