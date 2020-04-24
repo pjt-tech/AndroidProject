326585a8ccd32dde34c62865cddbd9a21a9f7b29
unknown <dktkd7044@gmail.com>
2020-04-24 11:31:16 +0900

    A

--- Chap_Firebase/app/src/main/java/com/kye/chap_firebase/MainActivity.java
+++ Chap_Firebase/app/src/main/java/com/kye/chap_firebase/MainActivity.java
@@ -1,14 +1,65 @@
 package com.kye.chap_firebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    EditText editText;
+
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        Button button = findViewById(R.id.button);
+
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("message");
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newMessage = editText.getText().toString();
+                reference.setValue(newMessage);
+
+
+
+            }
+        });
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                textView.setText(value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
