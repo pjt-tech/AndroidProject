b070e9492aa4749984f7ed0eeb7b323b73e3bc0d
unknown <dktkd7044@gmail.com>
2020-05-13 11:03:56 +0900

    a

--- MyScretMemo/app/src/main/java/com/kye/myscretmemo/MainActivity.java
+++ MyScretMemo/app/src/main/java/com/kye/myscretmemo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.kye.myscretmemo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,6 +27,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnPassword.setOnClickListener(this);
         btnFinish.setOnClickListener(this);
 
+        String passwordUseYn = loadPasswordUseYN(this);
+        if(passwordUseYn!=null && passwordUseYn.equals("Y")){
+            Intent intent = new Intent(getApplicationContext(),PasswordSettingActivity.class);
+            intent.putExtra("mode","unlock");
+            startActivityForResult(intent,1002);
+        }
+
     }
 
     public void onClick(View v){
@@ -30,10 +41,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(v==btnMemo){
 
         }else if(v==btnPassword){
-
+            Intent intent = new Intent(getApplicationContext(),PasswordSettingActivity.class);
+            intent.putExtra("mode","lock");
+            startActivityForResult(intent,1002);
 
         }else if(v==btnFinish){
             finish();
+        }
+    }
+
+    private String loadPasswordUseYN(Context context){
+        SharedPreferences preferences = context.getSharedPreferences("environ",0);
+
+        return preferences.getString("passwordUseYN","N");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(data!=null && requestCode==1002){
+            String mode = data.getStringExtra("mode");
+            if(mode!=null && mode.equals("exit")){
+                finish();
+            }
         }
     }
 }
--- MyScretMemo/app/src/main/java/com/kye/myscretmemo/PasswordSettingActivity.java
+++ MyScretMemo/app/src/main/java/com/kye/myscretmemo/PasswordSettingActivity.java
@@ -2,13 +2,117 @@ package com.kye.myscretmemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class PasswordSettingActivity extends AppCompatActivity {
+
+    EditText edtPassword1,edtPassword2;
+    CheckBox checkBox;
+    TextView txtTitle;
+    Intent intent;
+    String mode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_setting);
+
+        edtPassword1 = findViewById(R.id.editText4);
+        edtPassword2 = findViewById(R.id.editText5);
+
+        Button btnSave = findViewById(R.id.button8);
+        Button btnCancel = findViewById(R.id.button9);
+        txtTitle = findViewById(R.id.textView8);
+        checkBox = findViewById(R.id.checkBox);
+
+        intent = getIntent();
+        mode = intent.getStringExtra("mode");
+        if(mode!=null && mode.equals("lock")){
+            checkBox.setVisibility(View.GONE);
+            txtTitle.setText("비밀번호 설정");
+            btnSave.setText("저장");
+            btnCancel.setText("취소");
+        }else{
+            checkBox.setVisibility(View.VISIBLE);
+            txtTitle.setText("비밀번호 확인");
+            btnSave.setText("확인");
+            btnCancel.setText("끝내기");
+        }
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String password1 = edtPassword1.getText().toString();
+                String password2 = edtPassword2.getText().toString();
+                if(!(password1.equals(password2))){
+                    Toast.makeText(getApplicationContext(),"입력한 두 개의 비밀번호가 다릅니다.",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(mode!=null && mode.equals("lock")){
+                    savePasswordUseYN(getApplicationContext(),"Y");
+                    savePassword(getApplicationContext(),password1);
+                    Toast.makeText(getApplicationContext(),"비밀번호가 설정되었습니다.",Toast.LENGTH_LONG).show();
+                    finish();
+                }else if(mode!=null && mode.equals("unlock")){
+                    String prevPassword = loadPassword(getApplicationContext());
+                    if(password1.equals(prevPassword)){
+                        if(checkBox.isChecked()){
+                            savePasswordUseYN(getApplicationContext(),"N");
+                            savePassword(getApplicationContext(),"");
+                            Toast.makeText(getApplicationContext(),"비밀번호가 해제되었습니다",Toast.LENGTH_LONG).show();
+                            finish();
+                        }else {
+                           Toast.makeText(getApplicationContext(),"비밀번호가 확인되었습니다.",Toast.LENGTH_LONG).show();
+                           finish();
+                        }
+                    }else{
+                        Toast.makeText(getApplicationContext(),"비밀번호가 맞지 않습니다.",Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mode!=null && mode.equals("unlock")){
+                    intent.putExtra("mode","exit");
+                    setResult(RESULT_CANCELED,intent);
+                    finish();
+                }else {
+                    finish();
+                }
+            }
+        });
+
+    }
+
+    private void savePasswordUseYN(Context context,String useYN){
+        SharedPreferences preferences = context.getSharedPreferences("environ",0);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("passwordUseYN",useYN);
+        editor.commit();
+
+    }
+
+    private void savePassword(Context context,String password){
+        SharedPreferences preferences = context.getSharedPreferences("environ",0);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("password",password);
+        editor.commit();
+    }
+
+    private String loadPassword(Context context){
+        SharedPreferences preferences = context.getSharedPreferences("environ",0);
+        return preferences.getString("password","");
     }
 }
